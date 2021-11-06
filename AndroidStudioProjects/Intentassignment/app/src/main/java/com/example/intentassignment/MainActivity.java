2a3947968346c8b7e4aa853fe0281c15ae6d7960
mujtaba <mujtaba@salesassist.pk>
2021-11-06 16:26:46 +0500

    TODO TASK to make an intent that directs to repository DONE

--- AndroidStudioProjects/Intentassignment/app/src/main/java/com/example/intentassignment/MainActivity.java
+++ AndroidStudioProjects/Intentassignment/app/src/main/java/com/example/intentassignment/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.intentassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,12 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn=findViewById(R.id.url);
+        btn=findViewById(R.id.repo);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String url="https://github.com/mujtaba540/Intent-Assignment.git";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
             }
         });
     }
+
 }
